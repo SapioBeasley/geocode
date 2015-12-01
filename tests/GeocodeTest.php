87d2d94ede881e4e98d16a82450568f47ae907d3
Andreas Beasley <andreas@sapioweb.com>
2015-11-30 23:16:13 -0800

    tests passing

---
+++ tests/GeocodeTest.php
@@ -0,0 +1,20 @@
+<?php
+use Sapio\Geocode\GeoCodeController as Geocode;
+
+class GeocodeTest extends \PHPUnit_Framework_TestCase
+{
+
+  public function testGetCoordinate()
+  {
+    $address = '3330 florecita dr altadena ca 91101';
+
+    $result = Geocode::getCoordinates($address);
+
+    $expectedLat = '34.202516';
+    $expectedLng = '-118.162214';
+
+    // Test
+    $this->assertEquals($result['lat'], $expectedLat);
+    $this->assertEquals($result['lng'], $expectedLng);
+  }
+}
