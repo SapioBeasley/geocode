3dd10f6afb891514a9924ded4b881efacbf0a16e
Andreas Beasley <andreas@sapioweb.com>
2015-12-05 16:55:01 -0800

    Update GeocodeServiceProvider.php
    sapio to sapioweb

--- src/GeocodeServiceProvider.php
+++ src/GeocodeServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Sapio\Geocode;
+namespace Sapioweb\Geocode;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -23,6 +23,6 @@ class GeocodeServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->make('Sapio\Geocode\GeoCode');
+        $this->app->make('Sapioweb\Geocode\GeoCode');
     }
 }
